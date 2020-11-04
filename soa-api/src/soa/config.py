1e49435c1fc4feb371fb3f9a116cf20cb6d6dfef
luisblazquezm <luisblazquezm@usal.es>
2020-11-04 20:08:12 +0100

    [feat] Added endpoint for twitter extraction

--- soa-api/src/soa/config.py
+++ soa-api/src/soa/config.py
@@ -5,8 +5,25 @@
 
 import os
 
+##### General variables
 PORT = 5000
 HOST = '0.0.0.0'
 URL_PREFIX = '/soa/v1'
 DEBUG_MODE = True
+
+##### Covid variables
 BARCELONA_CKAN_TOKEN = "12552198a8992ac3b2129ebc803e29ac50515f383dfdc147abf39dd127050db1"
+
+##### Twitter variables
+# Variables that contains the credentials to access Twitter API
+ACCESS_TOKEN = '1252180895643111426-yOMCKE190xT3UljlDpmGAoTw3vmRyV'
+ACCESS_SECRET = 'DbXFYeUlPYAgWSfgnPGyzieRmfFSHk2M3WRm4EpSfAkXm'
+CONSUMER_KEY = 'jL9ufYzLGl6ezNygFGhts4uif'
+CONSUMER_SECRET = 'eqsQuOxClzrTjmEQcxtXbPkBVD4XrjM3EkYNqgsEcr53mzB3Xl'
+
+# Variables to use bearer token
+BEARER_TOKEN = "AAAAAAAAAAAAAAAAAAAAAHwMDwEAAAAAj5283nyjyrK%2BXzu5F01p%2Fx0b104%3DjCVTTywiqnH8C7XMqe60fEeXvaJbnpyoUOs140fLhIUutEgLsa"
+SEARCH_TWEETS_URI = 'https://api.twitter.com/1.1/search/tweets.json'
+
+DEFAULT_NUM_TWEETS_EXTRACTED = 300
+DEFAULT_TWEETS_LANGUAGE = 'en'
