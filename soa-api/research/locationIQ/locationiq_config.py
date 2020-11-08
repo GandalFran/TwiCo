3ad032df2c53df8ffd05337450315b0448ef7d48
Miguel <miguelcabezaspuerto@usal.es>
2020-11-08 17:51:58 +0100

    Miguel

---
+++ soa-api/research/locationIQ/locationiq_config.py
@@ -0,0 +1,7 @@
+#!flask/bin/python
+
+# Copyright 2020 Luis Blazquez Miñambres (@luisblazquezm), Miguel Cabezas Puerto (@MiguelCabezasPuerto) and Francisco Pinto-Santos (@gandalfran)
+# See LICENSE for details.
+
+# Variables that contains the credentials to access LocationIQ API
+ACCESS_TOKEN = 'pk.3086d0188b2fa4e26bcec2436d145356'
