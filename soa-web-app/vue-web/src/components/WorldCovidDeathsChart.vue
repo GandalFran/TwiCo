e850006a573f4c011d175fab0fa4cd87f3022339
Francisco Pinto Santos <franpintosantos@usal.es>
2020-11-29 20:58:34 +0100

    .

---
+++ soa-web-app/vue-web/src/components/WorldCovidDeathsChart.vue
@@ -0,0 +1,149 @@
+<template>
+	<div>
+		<v-progress-circular
+			v-show="dataAvailable === false"
+			class="circle-loading"
+			indeterminate
+			:size="300"
+			:width="20"
+			color="#F5F8FA"
+			/>
+		<div id="adchartdiv" v-show="dataAvailable === true" ref="adchartdiv"> </div>
+	</div>
+</template>
+
+<script>
+import Gradient from "javascript-color-gradient";
+import * as am4core from '@amcharts/amcharts4/core'
+import * as am4maps from '@amcharts/amcharts4/maps'
+import am4themesAnimated from '@amcharts/amcharts4/themes/animated'
+import am4geodataWorldLow from '@amcharts/amcharts4-geodata/worldLow'
+
+
+export default {
+	name: 'WorldCovidDeathsChart',
+	props: ['covidData'],
+	data: function () {
+		return {
+			chart: null,
+		};
+	},
+	methods: {
+		parseData: function (data) {
+			if(data === null || data === undefined){
+				return null;
+			}
+
+			// format data
+			data = data.map(function(e){
+				return{
+					deaths: e.deaths,
+					country: e.location.address.country,
+					id: e.location.address.country_code.toUpperCase()
+				};
+			});
+
+			// look for biggest value and calculate colors
+			var biggestValue = 0;
+			data.forEach(function(e){
+				if(e.deaths > biggestValue){
+					biggestValue = e.deaths;
+				}
+			});
+			
+			this.calculateColor(data, biggestValue);
+
+			return data;
+		},
+
+		calculateColor: function (data, biggestValue){
+			// configure gradient
+			const colorGradient = new Gradient();
+			colorGradient.setMidpoint(100);
+			colorGradient.setGradient("#E1E8ED", "#0A1E2B");
+			// build colors
+			data.forEach(function(e){
+				const index = Math.trunc(100 * e.deaths / biggestValue);
+				e.color = colorGradient.getColor(index);
+			})
+		},
+
+		createChart: function (data){
+			am4core.ready(function(){
+				// theme
+				am4core.useTheme(am4themesAnimated);
+
+				// Create map instance
+				var chart = am4core.create('adchartdiv', am4maps.MapChart);
+				chart.projection = new am4maps.projections.Miller();
+
+				// Create map polygon series for world map
+				var worldSeries = chart.series.push(new am4maps.MapPolygonSeries());
+				worldSeries.exclude = ["AQ"];
+				worldSeries.useGeodata = true;
+				worldSeries.geodata = am4geodataWorldLow;
+
+				var worldPolygon = worldSeries.mapPolygons.template;
+				worldPolygon.tooltipText = '{deaths} deaths in {name}';
+				worldPolygon.nonScalingStroke = true;
+				worldPolygon.strokeOpacity = 0.5;
+				worldPolygon.fill = am4core.color('#E1E8ED');
+				worldPolygon.propertyFields.fill = 'color';
+
+				// add data
+				worldSeries.data = data;
+
+				// add zoom control
+				chart.zoomControl = new am4maps.ZoomControl();
+				var homeButton = new am4core.Button();
+				homeButton.events.on('hit', function () {
+					worldSeries.show();
+					chart.goHome();
+				})
+				
+				// add home button
+				homeButton.icon = new am4core.Sprite();
+				homeButton.padding(7, 5, 7, 5);
+				homeButton.width = 30;
+				homeButton.icon.path = 'M16,8 L14,8 L14,16 L10,16 L10,10 L6,10 L6,16 L2,16 L2,8 L0,8 L8,0 L16,8 Z M16,8';
+				homeButton.marginBottom = 10;
+				homeButton.parent = chart.zoomControl;
+				homeButton.insertBefore(chart.zoomControl.plusButton);
+			});
+		},
+
+		destroyChart: function() {
+			if(this.chart !== null){
+				this.chart.dispose();
+			}
+		},
+
+		replaceChart: function(chart) {
+			this.destroyChart();
+			this.chart = chart;
+		},
+	},
+	computed: {
+		dataAvailable: function() {
+			this.$nextTick(() => {
+				const data = this.parseData(this.covidData);
+				this.createChart(data);
+			});
+			return (this.covidData !== null && this.covidData !== undefined);
+		}
+	},
+}
+</script>
+
+<style scoped>
+	.circle-loading {
+		display: block;
+		width: 100px;
+		margin: 0 auto;
+	}
+	#adchartdiv {
+		width: 100%;
+		height: 390px;
+		background: #F5F8FA;
+	}
+</style>
--- soa-web-app/vue-web/src/views/Dashboard.vue
+++ soa-web-app/vue-web/src/views/Dashboard.vue
@@ -63,16 +63,32 @@
 				:i="item.i"
 				:key="item.i"
 			>
-				<chart v-if="item.chartType == 'twitter'" :chartData="twitterData" > </chart>
-				<covid-chart v-if="item.chartType == 'covidWorld'" 
-					:covidData="covidDataWorld" 
-					:initialZoom="worldMapConfig.initialZoom" 
-					:initialCoordinates="worldMapConfig.initialCoordinates"
+				<world-covid-cases-chart 
+					v-if="item.chartType == 'world-covid-cases-chart'" 
+					:covidData="covidDataWorld"
 				/>
-				<covid-chart v-if="item.chartType == 'covidBarcelona'" 
+
+				<world-covid-deaths-chart 
+					v-if="item.chartType == 'world-covid-deaths-chart'" 
+					:covidData="covidDataWorld"
+				/>
+
+				<bareclona-covid-cases-chart 
+					v-if="item.chartType == 'barcelona-covid-cases-chart'" 
 					:covidData="covidDataBarcelona" 
 					:initialZoom="barcelonaMapConfig.initialZoom" 
 					:initialCoordinates="barcelonaMapConfig.initialCoordinates"
+				/>
+
+				<bareclona-covid-deaths-chart 
+					v-if="item.chartType == 'barcelona-covid-deaths-chart'" 
+					:covidData="covidDataBarcelona" 
+					:initialZoom="barcelonaMapConfig.initialZoom" 
+					:initialCoordinates="barcelonaMapConfig.initialCoordinates"
+				/>
+
+				<chart 
+					v-if="item.chartType == 'twitter'" :chartData="twitterData"
 				/>
 			</grid-item>
 		</grid-layout>
@@ -81,15 +97,22 @@
 
 <script>
 import axios from "axios";
-import Chart from "@/components/Chart.vue";
-import CovidChart from "@/components/CovidChart.vue";
 import VueGridLayout from 'vue-grid-layout';
+
+import Chart from "@/components/Chart.vue";
+import WorldCovidCasesChart from "@/components/WorldCovidCasesChart.vue";
+import WorldCovidDeathsChart from "@/components/WorldCovidDeathsChart.vue";
+import BarcelonaCovidCasesChart from "@/components/BarcelonaCovidCasesChart.vue";
+import BarcelonaCovidDeathsChart from "@/components/BarcelonaCovidDeathsChart.vue";
 
 export default {
 	name: 'Dashboard',
 	components: {
 		'chart':Chart,
-		CovidChart,
+		'world-covid-cases-chart': WorldCovidCasesChart,
+		'world-covid-deaths-chart': WorldCovidDeathsChart,
+		'bareclona-covid-cases-chart': BarcelonaCovidCasesChart,
+		'bareclona-covid-deaths-chart': BarcelonaCovidDeathsChart,
 		GridLayout: VueGridLayout.GridLayout,
 		GridItem: VueGridLayout.GridItem
 	},
@@ -101,17 +124,15 @@ export default {
 			covidDataBarcelona: null,
 			twitterData: null,
 			layout: [
-				{"x":0,"y":0,"w":5,"h":20,"i":"0", "chartType": "covidWorld"},
-				{"x":5,"y":0,"w":5,"h":20,"i":"1", "chartType": "covidBarcelona"},
-				{"x":0,"y":20,"w":10,"h":20,"i":"2", "chartType": "twitter"},
+				{"x":0,"y":0,"w":5,"h":20,"i":"0", "chartType": "world-covid-cases-chart"}, 
+				{"x":5,"y":0,"w":5,"h":20,"i":"1", "chartType": "barcelona-covid-cases-chart"},
+				{"x":0,"y":20,"w":5,"h":20,"i":"2", "chartType": "world-covid-deaths-chart"}, 
+				{"x":5,"y":20,"w":5,"h":20,"i":"3", "chartType": "barcelona-covid-deaths-chart"},
+				{"x":0,"y":40,"w":10,"h":20,"i":"4", "chartType": "twitter"},
 			],
 			barcelonaMapConfig: {
 				initialZoom: 7,
 				initialCoordinates: [2.021426, 41.560819],
-			},
-			worldMapConfig: {
-				initialZoom: 2,
-				initialCoordinates: [0, 0],
 			},
 		}
 	},
@@ -173,7 +194,7 @@ export default {
 	}
 
 	#title {
-		color: #1DA1F2;  
+		color: #1DA1F2;	
 		font-family: "Helvetica Neue", Roboto, "Segoe UI", Calibri, sans-serif;
 		font-weight: bold;
 	}
